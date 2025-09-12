e6d1b5d21cd530f28dbd74dd5751e3be1ca58947
infortechray <rayonireuben@duck.com>
2025-09-12 20:09:34 +0300

    Update vip-manager.sh

--- vip-manager.sh
+++ vip-manager.sh
@@ -388,6 +388,7 @@ show_menu() {
     echo -e "25) Domain Management"
     echo -e "26) user panel" 
     echo -e "27) host scanner"
+    echo -e "28) 😁 
     echo -e "${BLUE}========================================${NC}"
 }
 
@@ -497,6 +498,8 @@ while true; do
         26) sudo "$SCRIPTS_DIR/user-panel.sh"
 ‎   read -p "Press Enter to continue..." ;;
         27) sudo "$SCRIPTS_DIR/host-scanner.sh"
+‎   read -p "Press Enter to continue..." ;;
+        28) sudo "$cli_DIR/vip-panel"
 ‎   read -p "Press Enter to continue..." ;;
         *) print_status "ERROR" "Invalid option!"
            sleep 1 ;;
